1c092d7ae5f325ba1d03ab4cfaa5bd1dcfc0a874
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-10 23:28:02 +0300

    Add Header component

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,19 @@
   <div class="container">
     <div class="row">
       <div class="col-sm-12">
-
+        <Header></Header>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Header from "./components/Header";
+export default {
+  components: {
+    Header
+  }
+};
 </script>
 
 <style>
