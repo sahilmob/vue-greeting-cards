6a60e79e30e9cdc3a6071e9869c698639f6e78a6
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-11 15:10:08 +0300

    Add TextOutput component

--- src/components/card/CardFront.vue
+++ src/components/card/CardFront.vue
@@ -1,20 +1,33 @@
 <template>
   <div class="row">
     <div class="col-sm-6 card edit-area">
-      <cc-text-input></cc-text-input>
-      <cc-text-input></cc-text-input>
-      <cc-text-input></cc-text-input>
+      <cc-text-input @displayTextChanged="textBox1 = $event"></cc-text-input>
+      <cc-text-input @displayTextChanged="textBox2 = $event"></cc-text-input>
+      <cc-text-input @displayTextChanged="textBox3 = $event"></cc-text-input>
     </div>
-    <div class="col-sm-6 card card-display"></div>
+    <div class="col-sm-6 card card-display">
+      <cc-text-output></cc-text-output>
+      <cc-text-output></cc-text-output>
+      <cc-text-output></cc-text-output>
+    </div>
   </div>
 </template>
 
 <script>
 import TextInput from "./TextInput";
+import TextOutput from "./TextOutput";
 
 export default {
   components: {
-    ccTextInput: TextInput
+    ccTextInput: TextInput,
+    ccTextOutput: TextOutput
+  },
+  data() {
+    return {
+      textBox1: "",
+      textBox2: "",
+      textBox3: ""
+    };
   }
 };
 </script>
---
+++ src/components/card/TextOutput.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <p>Sample text</p>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
