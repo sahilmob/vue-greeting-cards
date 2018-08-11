c5333b6236dc77e6a504a0fd70fcfb6d4a2091ef
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-11 14:59:38 +0300

    Add TextInput component

---
+++ src/components/card/TextInput.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="form-group">
+                <h4>Edit Text:</h4>
+                <textarea class="form-control" cols="50" rows="4" placeholder="Add text here" v-model="textBoxInput" @keyup="textChanged"></textarea>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      textBoxInput: ""
+    };
+  },
+  methods: {
+    textChanged() {
+      this.$emit("displayTextChanged", this.textBoxInput);
+    }
+  }
+};
+</script>
