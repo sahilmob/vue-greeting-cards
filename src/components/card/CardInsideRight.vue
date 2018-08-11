0c7ce3c6295a944c1480fed04cf39f1f31cd7283
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-11 22:53:51 +0300

    Finishing card inside right section

--- src/components/card/CardInsideRight.vue
+++ src/components/card/CardInsideRight.vue
@@ -1,25 +1,48 @@
 <template>
-    <div class="row">
-        <div class="col-sm-6 card edit-area"></div>
-        <div class="col-sm-6 card card-display"></div>
+  <div class="row">
+    <div class="col-sm-6 card edit-area">
+      <cc-text-input @displayTextChanged="textBox1 = $event"></cc-text-input>
+      <cc-text-input @displayTextChanged="textBox2 = $event"></cc-text-input>
+      <cc-text-input @displayTextChanged="textBox3 = $event"></cc-text-input>
     </div>
+    <div class="col-sm-6 card card-display">
+      <cc-text-output :displayText="textBox1" :containerHeight="130"></cc-text-output>
+      <cc-text-output :displayText="textBox2" :containerHeight="130"></cc-text-output>
+      <cc-text-output :displayText="textBox3" :containerHeight="130"></cc-text-output>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import TextInput from "./TextInput";
+import TextOutput from "./TextOutput";
+export default {
+  components: {
+    ccTextInput: TextInput,
+    ccTextOutput: TextOutput
+  },
+  data() {
+    return {
+      textBox1: "",
+      textBox2: "",
+      textBox3: "",
+      imageName: ""
+    };
+  }
+};
 </script>
 
 <style scoped>
 .edit-area {
   background: #dffdff;
   padding: 20px;
-  height: 800px;
+  height: 500px;
   border: none;
 }
 
 .card-display {
   padding: 20px;
-  height: 800px;
+  height: 500px;
   border: none;
 }
 </style>
